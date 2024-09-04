978f9ad9596c4511c02ca10ff7581cda4402c6cd
sergesoroka <sergesoroka@gmail.com>
2024-09-04 07:26:11 +0200

    Download the file button

--- components/h5web/h5web.jsx
+++ components/h5web/h5web.jsx
@@ -21,11 +21,34 @@ export default function H5web({ domain }) {
   const initialPath = initialPathParams ? initialPathParams :
     hash ? hash : '/'
 
-
+  const downloadFile = () => {
+    fetch(`${import.meta.env.VITE_BaseURL}download?what=h5&domain=${domain}`)
+    .then(resp => resp.blob())
+      .then(blob => {
+        const url = window.URL.createObjectURL(blob);
+        const a = document.createElement("a");
+        a.style.display = "none";
+        a.href = url;
+        a.download = `${domain}`;
+        document.body.appendChild(a);
+        a.click();
+        window.URL.revokeObjectURL(url);
+        
+      })
+      .catch(() => alert("oh no!"));
+}
 
   return (
     <div>
-      <div style={{ height: "90vh" }}>
+      <div className="downloadBtn">
+        <button
+          className="shareBtn"
+          onClick={() => downloadFile()}
+        >
+          Download the file
+        </button>
+      </div>
+      <div >
         <HsdsProvider
           url="https://hsds-kc.ideaconsult.net"
           username="system-public-user"
