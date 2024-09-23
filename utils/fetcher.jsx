1a684bfa349edc47aedfaca0ba97accb3d394da9
sergesoroka <sergesoroka@gmail.com>
2024-09-23 20:16:14 +0300

    Update Token if status code is 401 or 403

--- utils/fetcher.jsx
+++ utils/fetcher.jsx
@@ -2,12 +2,30 @@ import keycloak from "./keycloak";
 
 const stored_token = localStorage.getItem("token")
 
-
-const fetcher = (url) => fetch(url, {
+const fetcher = async url => {
+  const res = await fetch(url, {
     headers: {
       Authorization: `Bearer ${stored_token}`
     }
-  }).then((res) => res.json());
+  })
+  if(res.status === 401 || res.status === 403) {
+    try {
+      await keycloak.updateToken(30);
+    } catch (error) {
+      console.error('Failed to refresh token:', error);
+    }
+  }
+ 
+  if (!res.ok) {
+    const error = new Error('An error occurred while fetching the data.')
+    // Attach extra info to the error object.
+    error.info = await res.json()
+    error.status = res.status
+    throw error
+  }
+ 
+  return res.json()
+}
 
 
   export default fetcher;
