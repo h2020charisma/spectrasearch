8eb05d3bdb6f1ae6aa9e2c6d55b15ecb0a6db145
sergesoroka <sergesoroka@gmail.com>
2024-09-30 11:17:24 +0300

    useFetch Hook for data fetching

--- components/Header/Header.jsx
+++ components/Header/Header.jsx
@@ -3,44 +3,55 @@ import { useNavigate } from "react-router-dom";
 import { useKeycloak } from "@react-keycloak/web";
 
 export default function Header() {
-  const { keycloak } = useKeycloak()
+  const { keycloak } = useKeycloak();
   const navigate = useNavigate();
 
-  
   if (keycloak.authenticated) {
-    localStorage.setItem("refreshToken", keycloak.refreshToken)
-    localStorage.setItem("token", keycloak.token)
-    localStorage.setItem("username", keycloak.tokenParsed.preferred_username)
+    localStorage.setItem("refreshToken", keycloak.refreshToken);
+    localStorage.setItem("token", keycloak.token);
+    localStorage.setItem("username", keycloak.tokenParsed.preferred_username);
   }
-  
-  const username = keycloak.tokenParsed?.preferred_username 
-      ? keycloak.tokenParsed?.preferred_username 
-      : localStorage.getItem("username");
+
+  // console.log("HEADER", keycloak.authenticated);
+
+  const username = keycloak.tokenParsed?.preferred_username
+    ? keycloak.tokenParsed?.preferred_username
+    : localStorage.getItem("username");
 
   const logoutHandle = () => {
-    localStorage.removeItem("username")
-    localStorage.removeItem("token")
-    localStorage.removeItem("refreshToken")
-  }
-  
-
+    localStorage.removeItem("username");
+    localStorage.removeItem("token");
+    localStorage.removeItem("refreshToken");
+  };
+  setInterval(async () => {
+    try {
+      await keycloak.updateToken(30);
+      console.log("Refreshed!");
+    } catch (error) {
+      console.log("Falied to refresh token..", error);
+    }
+  }, [30000]);
 
   return (
     <div className="logo">
       <h1 onClick={() => navigate("/")}>Raman spectra search</h1>
       <div className="usersInfo">
-        <div className="username">{username}</div>
-        {keycloak.authenticated || username ?
-          <button className="shareBtn" onClick={() => {
-            keycloak.logout()
-            logoutHandle()
-          }}>
+        <div className="username">{keycloak.authenticated && username}</div>
+        {keycloak.authenticated ? (
+          <button
+            className="shareBtn"
+            onClick={() => {
+              keycloak.logout();
+              logoutHandle();
+            }}
+          >
             Log out
-          </button> :
+          </button>
+        ) : (
           <button className="shareBtn" onClick={() => keycloak.login()}>
             Log in
           </button>
-        }
+        )}
       </div>
     </div>
   );
--- components/Investigations/Investigations.jsx
+++ components/Investigations/Investigations.jsx
@@ -1,24 +1,15 @@
-import useSWR from "swr";
-import Select from "../UI/Select";
 import SearchSelect from "../UI/SearchSelect";
 
-import fetcher from "../../utils/fetcher";
+import useFetch from "../../utils/useFetch";
 
-// eslint-disable-next-line react/prop-types
 export default function Investigations({
   reference,
   setReference,
   setImageSelected,
 }) {
-  const providerURL = `${
-    import.meta.env.VITE_BaseURL
-  }db/query/field?name=reference_s`;
+  const providerURL = `db/query/field?name=reference_s`;
 
-  const { data } = useSWR(providerURL, fetcher, {
-    revalidateIfStale: false,
-    revalidateOnFocus: false,
-    revalidateOnReconnect: false,
-  });
+  const { data, loading } = useFetch(providerURL);
 
   return (
     <div>
