4b178980e0702c617f936a1e5cfce05815d1aa88
sergesoroka <sergesoroka@gmail.com>
2024-10-05 16:33:32 +0300

    removed scope

--- src/main.jsx
+++ src/main.jsx
@@ -21,14 +21,11 @@ const stored_token = localStorage.getItem("token");
 const token = keycloak.token ? keycloak.token : stored_token;
 
 const base_url = import.meta.env.PROD ? "/search/worker.js" : "/worker.js";
-const reg_scope = import.meta.env.PROD ? "{ scope: '/search/' }" : null;
-
-console.log("base url", base_url);
 
 if ("serviceWorker" in navigator) {
   window.addEventListener("load", () => {
     navigator.serviceWorker
-      .register(base_url, reg_scope)
+      .register(base_url)
       .then((registration) => {
         console.log(
           "Service Worker registered with scope: ",
