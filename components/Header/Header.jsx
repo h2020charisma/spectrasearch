358c47b83f6382c27abeca5b4715df7a678669fd
sergesoroka <sergesoroka@gmail.com>
2024-11-07 13:37:00 +0200

    Added Help button

--- components/Header/Header.jsx
+++ components/Header/Header.jsx
@@ -1,14 +1,18 @@
 // import logo from "./../../public/charisma_logo.png";
-import { useNavigate } from "react-router-dom";
+import { Link, useNavigate } from "react-router-dom";
 
 export default function Header() {
   const navigate = useNavigate();
   return (
     <div className="logo">
-      {/* <div id="headerWrap">
-        <img src={logo} alt="logo" height={36} />
-      </div> */}
       <h1 onClick={() => navigate("/")}>Raman spectra search</h1>
+      <Link
+        to="https://docs.google.com/presentation/d/13oULefmyNbXaNhhmvrEQIrZaQ9DSCqBHNc-Wm2jRDP0/edit#slide=id.p"
+        target="_blank"
+        rel="noopener noreferrer"
+      >
+        <button className="shareBtn">Help</button>
+      </Link>
     </div>
   );
 }
