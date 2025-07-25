8554d88ca1fbc9debd84b0162d302ca4e6a3a55c
sergesoroka <sergesoroka@gmail.com>
2025-07-25 10:55:31 +0300

    Added Test: sources modal window

--- cypress/e2e/general.cy.js
+++ cypress/e2e/general.cy.js
@@ -1,4 +1,17 @@
+/* eslint-disable no-undef */
 const testURLRoot = "http://127.0.0.1:50722/search/";
+
+function setSourcesIntercepts() {
+  cy.intercept(
+    {
+      method: "GET",
+      url: "http://127.0.0.1:8000/db/query/sources",
+    },
+    {
+      fixture: "json/bk_rcapi_sources_generated.json",
+    }
+  ).as("getAllSources");
+}
 
 describe("General site functionality", () => {
   beforeEach(() => {
@@ -11,4 +24,16 @@ describe("General site functionality", () => {
       .and("be.visible");
   });
 
+  it("opens and closes the sources modal window", () => {
+    setSourcesIntercepts();
+    cy.get('[data-cy="preferences-btn"]').click();
+    cy.get('[id="radix-:r1:"]').should("be.visible");
+    cy.get(".selectOptionsSourses").find("*").should("exist");
+    cy.get(".sourceItem").first().click();
+    cy.get(".sourceItemLabel").should("be.visible");
+    cy.get("#cleanProject").click();
+    cy.get(".sourceItemLabel").should("not.exist");
+    cy.get('[data-cy="ok-btn"]').click();
+    cy.get('[id="radix-:r1:"]').should("not.exist");
+  });
 });
