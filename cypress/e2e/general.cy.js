e551b58b92ae3527ba35bf5ec2373c687e2f6014
sergesoroka <sergesoroka@gmail.com>
2025-08-27 13:34:35 +0300

    Added test for Preview modal window

--- cypress/e2e/general.cy.js
+++ cypress/e2e/general.cy.js
@@ -5,7 +5,7 @@ function setMainIntercepts() {
   cy.intercept(
     {
       method: "GET",
-      url: "http://127.0.0.1:8000/db/query?page=0&pagesize=30",
+      url: "http://localhost:8000/db/query?page=0&pagesize=30",
     },
     {
       fixture: "json/bk_rcapi_samples_generated.json",
@@ -29,7 +29,7 @@ function setDomainIntercepts() {
   cy.intercept(
     {
       method: "GET",
-      url: "http://127.0.0.1:8000/db/dataset?domain=%2FRRUF%2FAnatase__R060277-3__Raman__514__0__ccw__Raman_Data_Processed__14960.nxs%23%2FR060277%20Anatase_RRUF-4c1d6889-f9f1-5657-a80d-5738b50c4f9f%2FPROCESSED%2FR060277%20Anatase_1&values=True",
+      url: "http://localhost:8000/db/dataset?domain=%24FGFXL%5B_ps6M4wq%2C5jX_Fm%3EBC%3A8TocXe&values=True",
     },
     {
       fixture: "json/bk_rcapi_domain.json",
@@ -39,8 +39,8 @@ function setDomainIntercepts() {
 
 describe("General site functionality", () => {
   beforeEach(() => {
-    setMainIntercepts();
     cy.visit(testURLRoot);
+    setMainIntercepts();
   });
 
   it("displays the under development notice", () => {
@@ -49,29 +49,24 @@ describe("General site functionality", () => {
       .and("be.visible");
   });
 
-  it("opens and closes the sources modal window", () => {
-    setSourcesIntercepts();
-    cy.get('[data-cy="sources-btn"]').click();
-    cy.get('[id="radix-:r1:"]').should("be.visible");
-    cy.get(".selectOptionsSourses").find("*").should("exist");
-    cy.get(".sourceItem").first().click();
-    cy.get(".sourceItemLabel").should("be.visible");
-    cy.get("#cleanProject").click();
-    cy.get(".sourceItemLabel").should("not.exist");
-    cy.get('[data-cy="ok-btn"]').click();
-    cy.get('[id="radix-:r1:"]').should("not.exist");
-  });
+  // it("opens and closes the sources modal window", () => {
+  //   setSourcesIntercepts();
+  //   cy.get('[data-cy="sources-btn"]').click();
+  //   cy.get('[id="radix-:r1:"]').should("be.visible");
+  //   cy.get(".selectOptionsSourses").find("*").should("exist");
+  //   cy.get(".sourceItem").first().click();
+  //   cy.get(".sourceItemLabel").should("be.visible");
+  //   cy.get("#cleanProject").click();
+  //   cy.get(".sourceItemLabel").should("not.exist");
+  //   cy.get('[data-cy="ok-btn"]').click();
+  //   cy.get('[id="radix-:r1:"]').should("not.exist");
+  // });
 
-  it("opens and closes the domain modal window", () => {
+  it("opens and closes the Preview modal window", () => {
     setDomainIntercepts();
     cy.get('[data-cy="preview-btn"]').first().click();
-    // cy.get('[id="radix-:r1:"]').should("be.visible");
-    // cy.get(".selectOptionsDomain").find("*").should("exist");
-    // cy.get(".domainItem").first().click();
-    // cy.get(".domainItemLabel").should("be.visible");
-    // cy.get("#cleanProject").click();
-    // cy.get(".domainItemLabel").should("not.exist");
-    // cy.get('[data-cy="ok-btn"]').click();
-    // cy.get('[id="radix-:r1:"]').should("not.exist");
+    cy.get(".chart").should("be.visible");
+    cy.get(".chart").type("{esc}");
+    cy.get(".chart").should("not.exist");
   });
 });
