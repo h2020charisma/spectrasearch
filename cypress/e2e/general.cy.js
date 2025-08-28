8edf52380ed5b7bfcaf8dd295e00c44e6b194d94
sergesoroka <sergesoroka@gmail.com>
2025-08-28 16:12:43 +0300

    Added test for Search by Sample and Provider Widgets

--- cypress/e2e/general.cy.js
+++ cypress/e2e/general.cy.js
@@ -17,7 +17,7 @@ function setSourcesIntercepts() {
   cy.intercept(
     {
       method: "GET",
-      url: "http://127.0.0.1:8000/db/query/sources",
+      url: "http://localhost:8000/db/query/sources",
     },
     {
       fixture: "json/bk_rcapi_sources_generated.json",
@@ -29,12 +29,36 @@ function setDomainIntercepts() {
   cy.intercept(
     {
       method: "GET",
-      url: "http://localhost:8000/db/dataset?domain=%24FGFXL%5B_ps6M4wq%2C5jX_Fm%3EBC%3A8TocXe&values=True",
+      url: "http://localhost:8000/db/dataset?domain=Neon&values=True",
     },
     {
       fixture: "json/bk_rcapi_domain.json",
     }
-  ).as("getAllSources");
+  ).as("getAllDomains");
+}
+
+function setSampleIntercepts() {
+  cy.intercept(
+    {
+      method: "GET",
+      url: "http://localhost:8000/db/query/field?name=publicname_s",
+    },
+    {
+      fixture: "json/bk_rcapi_publicname_s.json",
+    }
+  ).as("getAllSamples");
+}
+
+function setProviderIntercepts() {
+  cy.intercept(
+    {
+      method: "GET",
+      url: "http://localhost:8000/db/query/field?name=reference_owner_s",
+    },
+    {
+      fixture: "json/bk_rcapi_reference_owner_s.json",
+    }
+  ).as("getAllSamples");
 }
 
 describe("General site functionality", () => {
@@ -49,18 +73,15 @@ describe("General site functionality", () => {
       .and("be.visible");
   });
 
-  // it("opens and closes the sources modal window", () => {
-  //   setSourcesIntercepts();
-  //   cy.get('[data-cy="sources-btn"]').click();
-  //   cy.get('[id="radix-:r1:"]').should("be.visible");
-  //   cy.get(".selectOptionsSourses").find("*").should("exist");
-  //   cy.get(".sourceItem").first().click();
-  //   cy.get(".sourceItemLabel").should("be.visible");
-  //   cy.get("#cleanProject").click();
-  //   cy.get(".sourceItemLabel").should("not.exist");
-  //   cy.get('[data-cy="ok-btn"]').click();
-  //   cy.get('[id="radix-:r1:"]').should("not.exist");
-  // });
+  it("opens and closes the sources modal window", () => {
+    setSourcesIntercepts();
+    cy.get('[data-cy="sources-btn"]').click();
+    cy.get(".sourceName").should("be.visible");
+    cy.get(".sourceName").click();
+    cy.get(".sourceNameLabel").should("contain.text", "charisma");
+    cy.get('[data-cy="ok-btn"]').click();
+    // setMainIntercepts();
+  });
 
   it("opens and closes the Preview modal window", () => {
     setDomainIntercepts();
@@ -68,5 +89,27 @@ describe("General site functionality", () => {
     cy.get(".chart").should("be.visible");
     cy.get(".chart").type("{esc}");
     cy.get(".chart").should("not.exist");
+  });
+
+  it("opens Search by Sample Widget and looks for the sample", () => {
+    setSampleIntercepts();
+    cy.get('[data-cy="search-by-sample"]').click();
+    cy.get('[data-cy="samples"]').should("be.visible");
+    cy.get('[data-cy="samples"]').click();
+    setMainIntercepts();
+    cy.contains("Neon").click();
+    cy.get(".metadataInfoValue").should("contain.text", "Neon");
+    cy.get('[data-cy="close-badge-btn"]').click();
+  });
+
+  it("opens Search by Provider Widget and looks for the provider", () => {
+    setProviderIntercepts();
+    cy.get('[data-cy="search-by-data-provider"]').click();
+    cy.get('[data-cy="providers"]').should("be.visible");
+    cy.get('[data-cy="providers"]').click();
+    setMainIntercepts();
+    cy.contains("RRUFF").click();
+    cy.get(".metadataInfoValue").should("contain.text", "RRUFF");
+    cy.get('[data-cy="close-badge-btn"]').click();
   });
 });
